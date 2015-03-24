1dd89104e2b7a227bf64db0535f503f5093cc05b
Tiago Fernandes <ei11054@fe.up.pt>
2015-03-24 15:12:42 +0000

    first commit

---
+++ Common/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Class1
+    {
+    }
+
+    public interface IUser
+    {
+        bool LogUser(string nickname, string password);
+        bool RegisterUser(string nickname, string password, string name);
+    }
+}
