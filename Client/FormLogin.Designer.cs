a63c0bc45fc62c5641e27b2b0e42ae874ddd54da
Tiago Fernandes <ei11054@fe.up.pt>
2015-03-28 12:14:42 +0000

    implemented register

--- Client/FormLogin.Designer.cs
+++ Client/FormLogin.Designer.cs
@@ -33,13 +33,27 @@
             this.textUsername = new System.Windows.Forms.TextBox();
             this.textPassword = new System.Windows.Forms.TextBox();
             this.buttonLogin = new System.Windows.Forms.Button();
+            this.tabLogin = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textRpassword1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textRName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.textRpassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textRNickname = new System.Windows.Forms.TextBox();
+            this.tabLogin.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 52);
+            this.label1.Location = new System.Drawing.Point(33, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 0;
@@ -48,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 84);
+            this.label2.Location = new System.Drawing.Point(33, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 1;
@@ -56,53 +70,166 @@
             // 
             // textUsername
             // 
-            this.textUsername.Location = new System.Drawing.Point(121, 49);
+            this.textUsername.Location = new System.Drawing.Point(108, 23);
             this.textUsername.Name = "textUsername";
             this.textUsername.Size = new System.Drawing.Size(100, 20);
-            this.textUsername.TabIndex = 2;
+            this.textUsername.TabIndex = 1;
             // 
             // textPassword
             // 
-            this.textPassword.Location = new System.Drawing.Point(121, 81);
+            this.textPassword.Location = new System.Drawing.Point(108, 55);
             this.textPassword.Name = "textPassword";
             this.textPassword.PasswordChar = '*';
             this.textPassword.Size = new System.Drawing.Size(100, 20);
-            this.textPassword.TabIndex = 3;
+            this.textPassword.TabIndex = 2;
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(95, 129);
+            this.buttonLogin.Location = new System.Drawing.Point(76, 100);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(75, 23);
-            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.TabIndex = 3;
             this.buttonLogin.Text = "login";
             this.buttonLogin.UseVisualStyleBackColor = true;
             this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
+            // tabLogin
+            // 
+            this.tabLogin.Controls.Add(this.tabPage1);
+            this.tabLogin.Controls.Add(this.tabPage2);
+            this.tabLogin.Location = new System.Drawing.Point(12, 12);
+            this.tabLogin.Name = "tabLogin";
+            this.tabLogin.SelectedIndex = 0;
+            this.tabLogin.Size = new System.Drawing.Size(245, 179);
+            this.tabLogin.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonLogin);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textPassword);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textUsername);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(237, 153);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "login";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textRpassword1);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.textRName);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textRpassword);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textRNickname);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(237, 153);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "register";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textRpassword1
+            // 
+            this.textRpassword1.Location = new System.Drawing.Point(102, 88);
+            this.textRpassword1.Name = "textRpassword1";
+            this.textRpassword1.PasswordChar = '*';
+            this.textRpassword1.Size = new System.Drawing.Size(100, 20);
+            this.textRpassword1.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "password";
+            // 
+            // textRName
+            // 
+            this.textRName.Location = new System.Drawing.Point(102, 10);
+            this.textRName.Name = "textRName";
+            this.textRName.Size = new System.Drawing.Size(100, 20);
+            this.textRName.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(107, 174);
+            this.label3.Location = new System.Drawing.Point(27, 39);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "username";
+            // 
+            // textRpassword
+            // 
+            this.textRpassword.Location = new System.Drawing.Point(102, 62);
+            this.textRpassword.Name = "textRpassword";
+            this.textRpassword.PasswordChar = '*';
+            this.textRpassword.Size = new System.Drawing.Size(100, 20);
+            this.textRpassword.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "password";
+            // 
+            // textRNickname
+            // 
+            this.textRNickname.Location = new System.Drawing.Point(102, 36);
+            this.textRNickname.Name = "textRNickname";
+            this.textRNickname.Size = new System.Drawing.Size(100, 20);
+            this.textRNickname.TabIndex = 2;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 196);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.buttonLogin);
-            this.Controls.Add(this.textPassword);
-            this.Controls.Add(this.textUsername);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(269, 203);
+            this.Controls.Add(this.tabLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "FormLogin";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Login";
+            this.tabLogin.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -113,7 +240,19 @@
         private System.Windows.Forms.TextBox textUsername;
         private System.Windows.Forms.TextBox textPassword;
         private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TabControl tabLogin;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textRpassword1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textRName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textRpassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textRNickname;
+
 
     }
 }
--- Client/FormLogin.cs
+++ Client/FormLogin.cs
@@ -8,21 +8,20 @@ namespace Client
     public partial class FormLogin : Form
     {
         private IUser user;
+        private IUsers users;
 
         public FormLogin()
         {
             RemotingConfiguration.Configure("Client.exe.config", false);
+            users = (IUsers)RemoteNew.New(typeof(IUsers));
             InitializeComponent();
-
-
-
         }
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             if (textUsername.Text != "" && textPassword.Text != "")
             {
-                IUsers users = (IUsers)RemoteNew.New(typeof(IUsers));
+                
                 user = users.LogUser(textUsername.Text, textPassword.Text);
                 if (user != null)
                 {
@@ -41,5 +40,43 @@ namespace Client
                    MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var name = textRName.Text;
+            var nickname = textRNickname.Text;
+            var password = textRpassword.Text;
+            var password1 = textRpassword1.Text;
+
+            if (name == "" || nickname == "" || password == "" || password1 == "") //not everything fillen in
+            {
+                MessageBox.Show("You must fill in all fields!", "Warning", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            }
+            else if (password != password1) //passwords don't match
+            {
+                MessageBox.Show("Passwords don't match!", "Warning", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                textRpassword.Text = "";
+                textRpassword1.Text = "";
+            }
+            else //success
+            {
+                user = users.RegisterUser(nickname, password, name);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Username already taken! Choose another one!", "Warning", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                    textRNickname.Text = "";
+                }
+                else //register successful
+                {
+                    MessageBox.Show("You've registered with success!", "Welcome", MessageBoxButtons.OK,
+                     MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+
     }
 }
